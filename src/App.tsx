40ab0c0ada81d720f5660addfe2aac17128b45e7
Hyunjin <jake5007@naver.com>
2026-03-31 11:48:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className="underline text-3xl text-rose-300">Line Watch</div>
+      <button className="btn btn-xl btn-primary">button</button>
+    </>
+  );
+}
+
+export default App;
